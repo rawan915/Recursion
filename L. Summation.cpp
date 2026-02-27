593203ac2a336fbe403358c986f22789cd9f7295
Rawan Gamal <rg838836@gmail.com>
2026-02-28 01:00:38 +0200

    Add L. Summation

--- L. Summation.cpp
+++ L. Summation.cpp
@@ -1,21 +1,21 @@
-//#include<iostream>
-//using namespace std;
-//typedef long long ll;
-//
-//
-//ll  printSum(int n , ll arr[], int i) {
-//	if (i==n)
-//		return 0;
-//	return arr[i] + printSum(n, arr, i + 1);
-//}
-//
-//int main() {
-//
-//	int n; cin >> n;
-//	ll arr[1005];
-//	for (int i = 0; i < n; i++)
-//	{
-//		cin >> arr[i];
-//	}
-//	cout << printSum(n, arr , 0);
-//}
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+
+ll  printSum(int n , ll arr[], int i) {
+	if (i==n)
+		return 0;
+	return arr[i] + printSum(n, arr, i + 1);
+}
+
+int main() {
+
+	int n; cin >> n;
+	ll arr[1005];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << printSum(n, arr , 0);
+}
