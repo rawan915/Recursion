84b9c9b521775c7333715cd31fd08d2a91b45fc0
Rawan Gamal <rg838836@gmail.com>
2026-02-28 00:15:22 +0200

    Add C. Print from N to 1.cpp

---
+++ C. Print from N to 1.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//void print(int n) {
+//	if (n == 0)
+//		return;
+//	cout << n ;
+//	if (n != 1)
+//		cout << " ";
+//	print(n - 1);
+//	
+//
+//}
+//
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	print(n);
+//
+//}
