2697ad08efd9faa2c43c68b8c2752af9555071c8
Rawan Gamal <rg838836@gmail.com>
2026-02-28 00:15:40 +0200

    Add I. Count Vowels.cpp

---
+++ I. Count Vowels.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int countVowels(string s, int i ) {
+//	if (i==s.length())
+//		return 0;
+//	char c = s[i];
+//	if (c >= 'A' && c <= 'Z')
+//		c = c + 32;
+//	if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+//		return 1 + countVowels(s, i + 1);
+//	else
+//		return countVowels(s, i + 1);
+//
+//
+//}
+//
+//int main() {
+//	string s; 
+//	getline(cin, s);
+//	cout << countVowels(s , 0);
+//
+//}
+//
