3ff4f72d917f4f203defc1838048a71b1b72f391
Rawan Gamal <rg838836@gmail.com>
2026-02-28 00:15:26 +0200

    Add D. Print Digits using Recursion.cpp

---
+++ D. Print Digits using Recursion.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//void printDigits(int n) {
+//    if (n == 0)
+//        return;
+//
+//    int digits = n % 10;
+//    printDigits(n / 10);
+//
+//    cout << digits << " ";  
+//}
+//
+//int main() {
+//    int t;
+//    cin >> t;
+//    while (t--) {
+//        int n;
+//        cin >> n;
+//        if (n == 0)
+//            cout << "0 ";
+//        else
+//            printDigits(n);
+//        cout << endl;
+//    }
+//}
